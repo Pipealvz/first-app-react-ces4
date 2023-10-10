7ba6d97b643c945ad7fd52575d046699a2935a18
Felipe Alvarez <pipedim03@gmail.com>
2023-10-10 00:38:50 -0500

    Fix path image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { Link } from "react-router-dom";
-import ok from '../images/ok.webp'
+import ok from './images/ok.webp'
 
 function App() {
   //const [count, setCount] = useState(0)
