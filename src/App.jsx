ada6e5037058369913c75e7e8d711c621c248ce9
Felipe Alvarez <pipedim03@gmail.com>
2023-10-10 00:43:08 -0500

    Fix path Images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Link } from "react-router-dom";
-import ok from './images/ok.webp'
+import ok from '../images/ok.webp'
 
 function App() {
   //const [count, setCount] = useState(0)
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Swal from 'sweetalert2'
 import { v4 as uuidv4 } from 'uuid';
-import ok from './images/ok.webp'
+import ok from '../images/ok.webp'
 
 const TodoList = () => {
 
